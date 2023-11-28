7d45c026fc7b7ab6cb2d7668ca7d8514bdb4e3e5
Gustavo Pimenta <gustav.alves17@gmail.com>
2023-11-28 10:30:43 -0300

    labirinto e logica

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import os, time
+import pygame, sys, mazeLogic as mL
+from pygame.locals import QUIT
+
+pygame.init()
+width, height = 400, 300
+DISPLAYSURF = pygame.display.set_mode((width,height))
+pygame.display.set_caption('Hello world!')
+clock = pygame.time.Clock()
+
+#maze
+
 
 maze = []
 row = 0
@@ -18,136 +28,64 @@ tcol = 0
 #ordem: direita[R], esquerda[L], baixo[D] e cima[U](alterar depois).
 moveOrder = ['d','r','l','u']
 ourMoves = []
-def loadMaze():
-    os.system("clear")
-    f = open("maze.txt")
-    row = 0 
-    for line in f:
-        maze.append([])
-        col = 0
-        for c in line:
-            if c != '\n':
-                maze[row].append(int(c))
-                if int(c) == player:
-                    prow, pcol = row, col 
-                elif int(c) == target: 
-                    trow, tcol = row, col
-                col += 1 
-        row +=1
-    return (maze, row, col, prow, pcol, trow, tcol)    
+maze, row, col, prow, pcol, trow, tcol = mL.loadMaze()
 
-def printMaze(maze, row, col):
-    os.system("clear")
-    for r in range(row): 
-        for c in range(col): 
-            print(maze[r][c], end="")
-        print()
+mL.printMaze(maze, row, col)
 
-maze, row, col, prow, pcol, trow, tcol = loadMaze()
-printMaze(maze, row, col)
-print(row, col, prow, pcol, trow, tcol)
+cellWidth = width//col
+cellHeight = height//row
 
-
-#check valid move 
-
-
-def checkMoveDirection(maze, row, col, prow, pcol, direction):
-    nprow = prow 
-    npcol = pcol
-    if direction == 'd':
-        nprow +=1
-    elif direction == 'u':
-        nprow-=1
-    elif direction == 'r':
-        npcol +=1
-    elif direction == 'l':
-        npcol-=1   
-    if nprow < row and nprow >= 0 and npcol >= 0 and npcol < col and (maze[nprow][npcol] == 0 or maze[nprow][npcol] == target):
-        return True
-    return False
-# down           
-
-def moveDirection(maze, row, col, prow,pcol, direction):
-    maze[prow][pcol] = path
-    if direction == 'd':
-        prow+=1
-    elif direction == 'u':
-        prow-=1
-    elif direction == 'r':
-        pcol+=1
-    elif direction == 'l':
-        pcol-=1
-    maze[prow][pcol] = player
-    return (maze, prow, pcol)
-
-def undoMoveDirection(maze, row, col, prow,pcol, direction):
-    maze[prow][pcol] = deadend
-    if direction == 'd':
-        prow-=1
-    elif direction == 'u':
-        prow+=1
-    elif direction == 'r':
-        pcol-=1
-    elif direction == 'l':
-        pcol+=1
-    maze[prow][pcol] = player
-    return (maze, prow, pcol)
-
-def findMoveOrder(maze,row,col,prow, pcol,trow, tcol):
-    moveOrder = []
-    if prow < trow:
-        moveOrder.append('d')
-    else:
-        moveOrder.append('u')
-    if pcol <tcol:
-        moveOrder.append('r')
-    else:
-        moveOrder.append('l')
-    
-    while(len(moveOrder)<4):                #procurar saber o que faz essa parte do código(depois apagar esse comentário)
-        if not 'r' in moveOrder:
-            moveOrder.append('r')
-        if not 'd' in moveOrder:
-            moveOrder.append('d')
-        if not 'l' in moveOrder:
-            moveOrder.append('l')
-        if not 'u' in moveOrder:
-            moveOrder.append('u')
-    return moveOrder
-
+def drawMaze(maze,row,col):
+    for r in range(row):
+        for c in range(col):
+            cellColor = 'grey'
+            if maze[r][c] == player:
+                cellColor = 'blue' 
+            elif maze[r][c] == target:
+                cellColor = 'green'
+            elif maze[r][c] == wall:
+                cellColor = 'red'
+            elif maze[r][c] == deadend:
+                cellColor = 'black'
+            elif maze[r][c] == path:
+                cellColor = 'pink'
+            
+            pygame.draw.rect(DISPLAYSURF,cellColor, (c*cellWidth, r*cellHeight,cellWidth,cellHeight), width=cellWidth//4)  
 moveMade = False
-moveOrder = findMoveOrder(maze,row,col,prow, pcol,trow, tcol)
 while True:
+    DISPLAYSURF.fill('white')
+    drawMaze(maze,row,col)
+    
+    
     if prow == trow and pcol == tcol:
         print("Success 🐀")
         print(ourMoves, len(ourMoves))
         break
 
     moveMade = False
-
+    moveOrder = mL.findMoveOrder(maze, row, col, prow, pcol, trow, tcol)
     for move in moveOrder:
         print(move, ourMoves)
         #user_input = input()
         #time.sleep(0.4)
-
-        if checkMoveDirection(maze, row, col, prow, pcol, move):
-            maze, prow, pcol = moveDirection(maze, row, col, prow, pcol, move)
+        if mL.checkMoveDirection(maze, row, col, prow, pcol, move):
+            maze, prow, pcol = mL.moveDirection(maze, row, col, prow, pcol, move)
             ourMoves.append(move)
             moveMade = True
-            printMaze(maze, row, col)
+            mL.printMaze(maze, row, col)
+            drawMaze(maze,row,col)
             break
 
+
     if not moveMade:
-        maze, prow, pcol = undoMoveDirection(maze, row, col, prow, pcol, ourMoves.pop())
-        printMaze(maze, row, col)
-
-
+        maze, prow, pcol = mL.undoMoveDirection(maze, row, col, prow, pcol, ourMoves.pop())
+        mL.printMaze(maze, row, col)
+        drawMaze(maze,row,col)
+        
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+    clock.tick(10)
     
- 
- 
- 
- 
- 
- 
- 
- 
---
+++ mazeLogic.py
@@ -0,0 +1,153 @@
+import os, time
+
+maze = []
+row = 0
+col =0 
+
+player = 1
+wall = 2 
+path= 3
+deadend = 4
+target = 5
+
+prow = 0
+pcol = 0
+
+trow = 0
+tcol = 0
+#ordem: direita[R], esquerda[L], baixo[D] e cima[U](alterar depois).
+moveOrder = ['d','r','l','u']
+ourMoves = []
+def loadMaze():
+    os.system("clear")
+    f = open("maze.txt")
+    row = 0 
+    for line in f:
+        maze.append([])
+        col = 0
+        for c in line:
+            if c != '\n':
+                maze[row].append(int(c))
+                if int(c) == player:
+                    prow, pcol = row, col 
+                elif int(c) == target: 
+                    trow, tcol = row, col
+                col += 1 
+        row +=1
+    return (maze, row, col, prow, pcol, trow, tcol)    
+
+
+def printMaze(maze, row, col):
+    os.system("clear")
+    for r in range(row): 
+        for c in range(col): 
+            print(maze[r][c], end="")
+        print()
+
+#maze, row, col, prow, pcol, trow, tcol = loadMaze()
+#printMaze(maze, row, col)
+#print(row, col, prow, pcol, trow, tcol)
+
+
+#check valid move 
+
+def checkMoveDirection(maze, row, col, prow, pcol, direction):
+    nprow = prow 
+    npcol = pcol
+    if direction == 'd':
+        nprow +=1
+    elif direction == 'u':
+        nprow-=1
+    elif direction == 'r':
+        npcol +=1
+    elif direction == 'l':
+        npcol-=1   
+    if nprow < row and nprow >= 0 and npcol >= 0 and npcol < col and (maze[nprow][npcol] == 0 or maze[nprow][npcol] == target):
+        return True
+    return False
+# down           
+
+def moveDirection(maze, row, col, prow,pcol, direction):
+    maze[prow][pcol] = path
+    if direction == 'd':
+        prow+=1
+    elif direction == 'u':
+        prow-=1
+    elif direction == 'r':
+        pcol+=1
+    elif direction == 'l':
+        pcol-=1
+    maze[prow][pcol] = player
+    return (maze, prow, pcol)
+
+def undoMoveDirection(maze, row, col, prow,pcol, direction):
+    maze[prow][pcol] = deadend
+    if direction == 'd':
+        prow-=1
+    elif direction == 'u':
+        prow+=1
+    elif direction == 'r':
+        pcol-=1
+    elif direction == 'l':
+        pcol+=1
+    maze[prow][pcol] = player
+    return (maze, prow, pcol)
+
+def findMoveOrder(maze,row,col,prow, pcol,trow, tcol):
+    moveOrder = []
+    if prow < trow:
+        moveOrder.append('d')
+    else:
+        moveOrder.append('u')
+    if pcol <tcol:
+        moveOrder.append('r')
+    else:
+        moveOrder.append('l')
+    
+    while(len(moveOrder)<4):                #procurar saber o que faz essa parte do código(depois apagar esse comentário)
+        if not 'r' in moveOrder:
+            moveOrder.append('r')
+        if not 'd' in moveOrder:
+            moveOrder.append('d')
+        if not 'l' in moveOrder:
+            moveOrder.append('l')
+        if not 'u' in moveOrder:
+            moveOrder.append('u')
+    return moveOrder
+'''
+moveMade = False
+moveOrder = findMoveOrder(maze,row,col,prow, pcol,trow, tcol)
+while True:
+    if prow == trow and pcol == tcol:
+        print("Success 🐀")
+        print(ourMoves, len(ourMoves))
+        break
+
+    moveMade = False
+
+    for move in moveOrder:
+        print(move, ourMoves)
+        #user_input = input()
+        #time.sleep(0.4)
+
+        if checkMoveDirection(maze, row, col, prow, pcol, move):
+            maze, prow, pcol = moveDirection(maze, row, col, prow, pcol, move)
+            ourMoves.append(move)
+            moveMade = True
+            printMaze(maze, row, col)
+            break
+
+    if not moveMade:
+        maze, prow, pcol = undoMoveDirection(maze, row, col, prow, pcol, ourMoves.pop())
+        printMaze(maze, row, col)
+'''
+
+    
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
